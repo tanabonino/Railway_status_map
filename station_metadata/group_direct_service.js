3a5b92fbec9a6082bb4a5adf4a50c78a03e43cf9
tanabonino <tanabonino@hotmail.co.jp>
2026-02-21 14:40:07 +0900

    Complete direct-service station metadata coverage

---
+++ station_metadata/group_direct_service.js
@@ -0,0 +1,58 @@
+// 直通サービス系メタデータ（上野東京ライン / 湘南新宿ライン / 相鉄線直通列車）
+window.stationMetadataChunks = window.stationMetadataChunks || {};
+Object.assign(window.stationMetadataChunks, {
+  "久喜": {
+    nameKana: "くき",
+    interchanges: [
+      { toOperator: "東武", toLine: "伊勢崎線" }
+    ]
+  },
+  "小山": {
+    nameKana: "おやま"
+  },
+  "宇都宮": {
+    nameKana: "うつのみや",
+    interchanges: [
+      { toOperator: "宇都宮ライトレール", toLine: "宇都宮芳賀ライトレール線" }
+    ]
+  },
+  "桶川": {
+    nameKana: "おけがわ"
+  },
+  "熊谷": {
+    nameKana: "くまがや",
+    interchanges: [
+      { toOperator: "秩父鉄道", toLine: "秩父本線" }
+    ]
+  },
+  "鎌倉": {
+    nameKana: "かまくら",
+    interchanges: [
+      { toOperator: "江ノ島電鉄", toLine: "江ノ島電鉄線" }
+    ]
+  },
+  "逗子": {
+    nameKana: "ずし",
+    interchanges: [
+      { toOperator: "京急", toLine: "逗子線", toStationName: "逗子・葉山" }
+    ]
+  },
+  "藤沢": {
+    nameKana: "ふじさわ",
+    interchanges: [
+      { toOperator: "小田急", toLine: "江ノ島線" },
+      { toOperator: "江ノ島電鉄", toLine: "江ノ島電鉄線" }
+    ]
+  },
+  "平塚": {
+    nameKana: "ひらつか"
+  },
+  "小田原": {
+    nameKana: "おだわら",
+    interchanges: [
+      { toOperator: "小田急", toLine: "小田原線" },
+      { toOperator: "箱根登山鉄道", toLine: "鉄道線" },
+      { toOperator: "伊豆箱根鉄道", toLine: "大雄山線" }
+    ]
+  }
+});
