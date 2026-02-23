a8ca645eb38a19fe70552fcebf24232ffe2d75f3
tanabonino <tanabonino@hotmail.co.jp>
2026-02-23 16:26:01 +0900

    Expand Tohoku line data: senseki-tohoku and joban

--- station_metadata/lines/tohoku_senseki_tohoku.js
+++ station_metadata/lines/tohoku_senseki_tohoku.js
@@ -1,8 +1,15 @@
 // 路線別メタデータ: 仙石東北ライン（東北）
 window.stationMetadataChunks = window.stationMetadataChunks || {};
 Object.assign(window.stationMetadataChunks, {
-  "仙台": {"nameKana":"せんだい","interchanges":[{"toOperator":"仙台市地下鉄","toLine":"南北線"},{"toOperator":"仙台市地下鉄","toLine":"東西線"}]},
-  "塩釜": {"nameKana":"しおがま"},
-  "高城町": {"nameKana":"たかぎまち"},
-  "石巻": {"nameKana":"いしのまき"},
+  "仙台": {"nameKana":"せんだい","interchanges":[{"toOperator":"JR東日本","toLine":"東北新幹線"},{"toOperator":"JR東日本","toLine":"東北本線"},{"toOperator":"JR東日本","toLine":"仙石線"},{"toOperator":"JR東日本","toLine":"仙山線"},{"toOperator":"JR東日本","toLine":"常磐線"}]},
+  "塩釜": {"nameKana":"しおがま","interchanges":[{"toOperator":"JR東日本","toLine":"東北本線"}]},
+  "松島": {"nameKana":"まつしま","interchanges":[{"toOperator":"JR東日本","toLine":"東北本線"}]},
+  "高城町": {"nameKana":"たかぎまち","interchanges":[{"toOperator":"JR東日本","toLine":"仙石線"}]},
+  "野蒜": {"nameKana":"のびる","interchanges":[{"toOperator":"JR東日本","toLine":"仙石線"}]},
+  "陸前小野": {"nameKana":"りくぜんおの","interchanges":[{"toOperator":"JR東日本","toLine":"仙石線"}]},
+  "矢本": {"nameKana":"やもと","interchanges":[{"toOperator":"JR東日本","toLine":"仙石線"}]},
+  "東矢本": {"nameKana":"ひがしやもと","interchanges":[{"toOperator":"JR東日本","toLine":"仙石線"}]},
+  "陸前赤井": {"nameKana":"りくぜんあかい","interchanges":[{"toOperator":"JR東日本","toLine":"仙石線"}]},
+  "蛇田": {"nameKana":"へびた","interchanges":[{"toOperator":"JR東日本","toLine":"仙石線"}]},
+  "石巻": {"nameKana":"いしのまき","interchanges":[{"toOperator":"JR東日本","toLine":"石巻線"}]},
 });
